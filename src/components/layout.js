93545d6ad3875bfade4b0f817b8ce224adffc2ba
Orlundo Hubbard <weareprojectcode@gmail.com>
2023-10-07 14:41:05 -0500

    more code

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,15 @@
+import { Inter } from 'next/font/google'
+import Header from './Header'
+
+const inter = Inter({
+    subsets: ['latin'],
+    variable: "--font-inter"
+})
+
 export default function AppLayout({ children }) {
     return (
-        <div className="min-h-screen flex flex-col">
-            <header>header</header>
+        <div className={`${inter.variable} font-sans min-h-screen flex flex-col`}>
+            <Header></Header>
             <main className="flex-grow bg-[#f7f7f7]">
                 {children}
             </main>
